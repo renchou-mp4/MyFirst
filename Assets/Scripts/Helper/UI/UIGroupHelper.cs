9ae49b27dfdf7e8c7b8d54dcb2a0462d931b80c8
尹新宇 <1296822630@qq.com>
2025-11-27 03:06:44 +0800

    UI模块尝试使用

--- Assets/Scripts/Helper/UI/UIGroupHelper.cs
+++ Assets/Scripts/Helper/UI/UIGroupHelper.cs
@@ -3,24 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityGameFramework.Runtime;
 
 public class UIGroupHelper : UIGroupHelperBase
 {
     private Canvas  _canvas;
+
+    public const int DepthFactor = 100;
     
     public override void SetDepth(int depth)
     {
+        _canvas.overrideSorting = true;
+        _canvas.sortingOrder = depth * DepthFactor;
     }
 
     private void Awake()
     {
         _canvas = this.GetOrAddComponent<Canvas>();
+        gameObject.GetOrAddComponent<GraphicRaycaster>();
     }
 
     private void Start()
     {
-        int a = 1;
-        a++;
+        var rectTransform = GetComponent<RectTransform>();
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.one;
+        rectTransform.anchoredPosition = Vector2.zero;
+        rectTransform.sizeDelta = Vector2.zero;
     }
+    
 }
