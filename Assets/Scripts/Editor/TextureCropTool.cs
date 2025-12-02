eefdbed634f4f66494b18bf16478dd92ad2f56e6
尹新宇 <1296822630@qq.com>
2025-12-02 21:01:20 +0800

    图片裁剪工具

--- Assets/Scripts/Editor/TextureCropTool.cs
+++ Assets/Scripts/Editor/TextureCropTool.cs
@@ -7,14 +7,19 @@ using UnityEngine;
 public class TextureCropTool : EditorWindow
 {
     //配置参数
-    private bool _processAlphaChannel = true;
-    private bool _preserveOriginal = true;
-    private bool _processMultiple = false;
-    private int _padding = 2;
-    private string _outputPath = "";
-    
     private Texture2D _oldTexture;
     private Texture2D _newTexture;
+    private Texture2D _lastTexture;
+
+    private TextureImporter _importer;
+    private const float _baseRect = 128;
+    private float _alphaThreshold = 0.01f;
+    private int _padding = 0;
+    private string _outputPath = "";
+    private Color _boundsColor = Color.green;
+    private Rect _newRect;
+    private Rect _oldRect;
+    private Rect _newBounds;
     
     
     [MenuItem("Tools/图片裁剪")]
@@ -26,7 +31,7 @@ public class TextureCropTool : EditorWindow
     private void OnGUI()
     {
         DrawSettings();
-        DrawAction();
+        DrawLayout();
     }
 
     private void DrawSettings()
@@ -34,61 +39,177 @@ public class TextureCropTool : EditorWindow
         EditorGUILayout.LabelField("图片裁剪设置",EditorStyles.boldLabel);
         EditorGUILayout.Space();
         
-        _processAlphaChannel = EditorGUILayout.Toggle("Process Alpha Channel", _processAlphaChannel);
-        _preserveOriginal = EditorGUILayout.Toggle("Preserve Original", _preserveOriginal);
-        _padding = EditorGUILayout.IntSlider("Padding", _padding, 0, 10);
+        _alphaThreshold = EditorGUILayout.Slider("透明阈值",_alphaThreshold,0f,10f);
+        _padding = EditorGUILayout.IntSlider("外边距",_padding, 0, 10);
+        _boundsColor = EditorGUILayout.ColorField("包围盒颜色",_boundsColor);
+        
         
         EditorGUILayout.Space();
         EditorGUILayout.LabelField("输出目录", _outputPath);
     }
 
-    private void DrawAction()
+    private void DrawLayout()
     {
-        _oldTexture = EditorGUILayout.ObjectField("选择图片：", _oldTexture, typeof(Texture2D), false) as Texture2D;
+        _oldTexture = _newTexture = EditorGUILayout.ObjectField("选择图片：", _oldTexture, typeof(Texture2D), false) as Texture2D;
+        if (_lastTexture == null || _lastTexture != _oldTexture)
+        {
+            _outputPath = AssetDatabase.GetAssetPath(_oldTexture);
+            _lastTexture = _oldTexture;
+            _newBounds = Rect.zero;
+        }
+        if (_oldTexture == null) return;
+        
+        EditorGUILayout.Separator();
+        GUILayout.Space(10);
+        GUILayout.BeginVertical();
+        
+        //原图宽高比
+        float whRatio = _oldTexture.width * 1.0f / _oldTexture.height;
+        
+        //原图
         GUILayout.BeginHorizontal();
         GUILayout.FlexibleSpace();
 
-        Rect oldRect = GUILayoutUtility.GetRect(256, 256);
-        if(_oldTexture is not null)
-            GUI.DrawTexture(oldRect, _oldTexture);
+        if (_oldTexture is not null)
+        {
+            EditorGUILayout.LabelField("原图：",GUILayout.MaxWidth(100));
+            _oldRect = GUILayoutUtility.GetRect(_baseRect * whRatio, _baseRect,GUILayout.ExpandWidth(false),GUILayout.ExpandHeight(false));
+            GUI.DrawTexture(_oldRect, _oldTexture);
+            GUILayout.BeginVertical();
+            EditorGUILayout.LabelField("显示大小：",$"{_oldRect.width} x {_oldRect.height}",GUILayout.MaxWidth(300));
+            EditorGUILayout.LabelField("图标大小：",$"{_oldTexture.width} x {_oldTexture.height}",GUILayout.MaxWidth(300));
+            GUILayout.EndVertical();
+        }
         
-        Rect newRect = GUILayoutUtility.GetRect(256, 256);
-        if(_newTexture is not null)
-            GUI.DrawTexture(newRect, _newTexture);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        GUILayout.Space(10);
+        
+        //预览图
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
+
+        if (_newTexture is not null)
+        {
+            EditorGUILayout.LabelField("预览图：",GUILayout.MaxWidth(100));
+            _newRect = GUILayoutUtility.GetRect(_baseRect * whRatio, _baseRect,GUILayout.ExpandWidth(false),GUILayout.ExpandHeight(false));
+            GUI.DrawTexture(_newRect, _newTexture);
+            GUILayout.BeginVertical();
+            EditorGUILayout.LabelField("显示大小：",$"{_newRect.width} x {_newRect.height}",GUILayout.MaxWidth(300));
+            EditorGUILayout.LabelField("图标大小：",$"{_newTexture.width} x {_newTexture.height}",GUILayout.MaxWidth(300));
+            GUILayout.EndVertical();
+        }
         
         GUILayout.FlexibleSpace();
         GUILayout.EndHorizontal();
         
+
+        //计算包围盒
         Handles.BeginGUI();
-        
+        Handles.color = _boundsColor;
+        if(GUILayout.Button("计算包围盒", GUILayout.Height(30)))
+        {
+            _newBounds = CalculateNewBounds();
+        }
+        if (_oldTexture != null && _newTexture != null)
+        {
+            Handles.DrawWireCube(_oldRect.center,new Vector2(_oldRect.width,_oldRect.height));
+            Handles.DrawWireCube(_newRect.center,new  Vector2(_newBounds.width * _oldRect.width / _oldTexture.width,_newBounds.height * _oldRect.height / _oldTexture.height));
+        }
         Handles.EndGUI();
+        
+        GUILayout.EndVertical();
     }
 
-    private const float _baseRect = 256;
-    private ValueTuple<float,float> _ratio;
+    private Rect CalculateNewBounds()
+    {
+        if (_newTexture == null) return Rect.zero;
+        
+        _importer = AssetImporter.GetAtPath(_outputPath) as TextureImporter;
+
+        if (_importer is { isReadable: false })
+        {
+            _importer.isReadable = true;
+            _importer.SaveAndReimport();
+        }
+
+        Color32[] pixels = _newTexture.GetPixels32();
+        int width = _newTexture.width;
+        int height = _newTexture.height;
+
+        int minX = width;
+        int minY = height;
+        int maxX = 0;
+        int maxY = 0;
+
+        //查找非透明像素边界
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                Color32 pixel = pixels[y*width + x];
+                if (pixel.a > _alphaThreshold*255) //非透明像素
+                {
+                    if (x < minX) minX = x;
+                    if (x > maxX) maxX = x;
+                    if (y < minY) minY = y;
+                    if (y > maxY) maxY = y;
+                }
+            }
+        }
+
+        if (_importer is {isReadable: true})
+        {
+            _importer.isReadable = false;
+            _importer.SaveAndReimport();
+        }
+        
+        //没有非透明像素，返回整个纹理
+        if(minX > maxX || minY > maxY)
+            return new Rect(0,0,width,height);
+        
+        //添加安全边界
+        minX = Mathf.Max(0, minX - 1);
+        minY = Mathf.Max(0, minY - 1);
+        maxX = Mathf.Min(width-1, maxX + 1);
+        maxY = Mathf.Min(height-1, maxY + 1);
+        
+        int cropWidth = maxX - minX + 1;
+        int cropHeight = maxY - minY + 1;
+        
+        return new Rect(minX,minY,cropWidth,cropHeight);
+    }
+
+    private void CropTexture()
+    {
+        if (_newBounds.width <= 0 || _newBounds.height <= 0)
+        {
+            Debug.Log($"裁剪失败！裁剪包围盒非法：{_newBounds.width} x {_newBounds.height}");
+            return;
+        }
+        
+        //创建新纹理
+        Texture2D croppedTexture = new Texture2D(
+            (int)_newBounds.width + _padding*2,
+            (int)_newBounds.height + _padding*2,
+            TextureFormat.RGBA32,
+            false);
+        
+        //填充透明背景
+        Color[]  pixels = new Color[croppedTexture.width * croppedTexture.height];
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            pixels[i] = Color.clear;
+        }
+        croppedTexture.SetPixels(pixels);
+
+        //复制有效区域
+
+        
+        
+    }
     
-    private void GetBox(Rect target,float width,float height,Rect oldRect)
-    {
-        float widthRatio = target.width / width;
-        float heightRatio = target.height / height;
-        
-    }
-
-    private Rect GetSetImageRect()
-    {
-        float heightRatio = _oldTexture.height / _baseRect;
-
-        if (heightRatio <= 1)
-        {
-            float h = 256;
-            float w = _baseRect/_oldTexture.width * _oldTexture.height;
-        }
-        else
-        {
-            float h = 256;
-            float w = _oldTexture.width / _baseRect * _oldTexture.width;
-        }
-        
-        return new Rect()
-    }
+    private void Get
+    
 }
